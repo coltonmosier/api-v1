9c9da954ad3630ca1bb0daf85f0e0bd2ae30d5e6
Colton Mosier <coltonmosier44@gmail.com>
2024-04-18 20:24:04 -0500

    help me

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -20,23 +20,26 @@ func (w *wrappedWriter) WriteHeader(status int) {
 	w.ResponseWriter.WriteHeader(status)
 }
 
-//var LogDB = database.InitLoggingDatabase()
+// var LogDB = database.InitLoggingDatabase()
 var LogFile = os.Getenv("LOG_FILE")
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// NOTE: I have to do this here bc it was overwriting the Content-Type header in the response
-        start := time.Now()
+		start := time.Now()
 		w.Header().Add("Content-Type", "application/json")
-        w.Header().Add("Access-Control-Allow-Origin", "*")
-        w.Header().Add("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS")
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS")
 		wr := &wrappedWriter{w, http.StatusOK}
 		next.ServeHTTP(wr, r)
-
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 		ip := strings.Split(r.Header.Get("X-Real-IP"), ":")[0]
-        if ip == "" {
-            ip = strings.Split(r.RemoteAddr, ":")[0]
-        }
+		if ip == "" {
+			ip = strings.Split(r.RemoteAddr, ":")[0]
+		}
 		msg := fmt.Sprintf("%s %d %s %s %v\n", ip, wr.status, r.Method, r.RequestURI, time.Since(start))
 		log.Print(msg)
 
